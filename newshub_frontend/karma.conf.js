0238b1252bd45ad84484f5ec82e53d78bbd21e68
ArsenMonets <bela.ozzik@gmail.com>
2026-06-06 22:37:55 +0300

    changed xml parser 2

--- newshub_frontend/karma.conf.js
+++ newshub_frontend/karma.conf.js
@@ -2,44 +2,60 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-junit-reporter'), // 👈 ОБОВ'ЯЗКОВО ДОДАЄМО СЮДИ
+      require('karma-sonarqube-reporter'), // 👈 Наш новий плагін
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    
     client: {
-      jasmine: {
-      },
+      jasmine: {},
       clearContext: false 
     },
+    
     jasmineHtmlReporter: {
       suppressAll: true 
     },
+    
+    // 1. Покриття коду відправляємо в окрему ізольовану папку
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/newshub-frontend'),
+      dir: require('path').join(__dirname, './coverage-sonar/lcov-report'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'lcovonly' },
+        { type: 'lcovonly' }, // генерує lcov.info
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
     
-    junitReporter: {
-      outputDir: require('path').join(__dirname, './coverage'), 
-      outputFile: 'junit-report.xml',
-      useBrowserName: false
+    // Активуємо обидва репортери
+    reporters: ['progress', 'kjhtml', 'sonarqube'],
+
+    // 2. Звіт про кількість тестів відправляємо в ІНШУ ізольовану папку
+    sonarqubeReporter: {
+      basePath: 'src',
+      outputFolder: require('path').join(__dirname, './coverage-sonar/tests-report'), // 👈 окрема підпапка
+      filePattern: '**/*.spec.ts',
+      encoding: 'utf-8',
+      outputFile: 'sonar-spec-reporter.xml',
+      legacyMode: false
     },
+    
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: true, // 👈 КРИТИЧНО для CI: Karma чекає завершення обох плагінів
+    restartOnFileChange: false,
+    
+    // Даємо асинхронним плагінам час записати файли на диск
+    browserNoActivityTimeout: 30000,
+    browserDisconnectTimeout: 10000,
+    pingTimeout: 10000
   });
 };
